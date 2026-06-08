5e179ae30b68a27fdafc47568c03543b6b6a2cc3
jesusmcortez553-gif <jesusmcortez553@gmail.com>
2026-06-07 23:28:35 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVfCIkS9YSR8QbpBog4AyM-CnqTm26ZUY",
