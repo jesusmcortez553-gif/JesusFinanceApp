51d1cd62b00659c9b07a8b392a4a7b1b7cfca6ce
jesusmcortez553-gif <jesusmcortez553@gmail.com>
2026-06-16 01:24:15 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import {
   AreaChart, Area, BarChart, Bar, PieChart, Pie, Cell,
   XAxis, YAxis, Tooltip, ResponsiveContainer
